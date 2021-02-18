1ac0806ab09ab531dff01e95cb8d1911b2c1f231
ndraa124 <indradavidpesik@gmail.com>
2021-02-18 14:09:45 +0800

    update config

--- config/api.config.js
+++ config/api.config.js
@@ -1,8 +1,8 @@
 const config = {}
 
-config.githubToken = '9d13015e284f79e4c1c4340afbab0b6fa2aa898c'
-config.githubUsername = 'ndraa124'
-config.githubClient = 'b9808ae9b5d7754bded3'
-config.githubSecret = 'a576edd92f39a8ca4e0992c74dbe1fe95abaca45'
+config.githubToken = 'your github token is needed'
+config.githubUsername = 'your github username'
+config.githubClient = 'yout github client id'
+config.githubSecret = 'your github client secret'
 
 module.exports = config
